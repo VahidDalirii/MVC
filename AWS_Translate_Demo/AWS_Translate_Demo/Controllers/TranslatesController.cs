4cfad3f83d7a80a073a2580884086e90f9061e1a
//-\|-|||) <vahiddaliri@gmail.com>
2020-03-09 10:24:17 +0100

    Update

---
+++ AWS_Translate_Demo/AWS_Translate_Demo/Controllers/TranslatesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Translate;
+using Amazon.Translate.Model;
+using AWS_Translate_Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWS_Translate_Demo.Controllers
+{
+    public class TranslatesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddComment()
+        {
+            var model = new TranslateCommentViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult TranslateComment(TranslateCommentViewModel comment)
+        {
+            var translate = new AmazonTranslateClient("AKIAX3JSZJ36DOCUNIJY", "vTKVVvQs01qY25LczK5uSdD3gev49cdXfKNr7LRT", RegionEndpoint.USEast1);
+            var request = new TranslateTextRequest() { Text = comment.CommentText, SourceLanguageCode = "en", TargetLanguageCode = comment.TargetLanguage };
+
+            var task = translate.TranslateTextAsync(request); //.TranslateText(request) // Make the actual call to Amazon Translate
+            task.Wait();
+
+            var model = new TranslatedCommentViewModel()
+            {
+                CommentText = comment.CommentText,
+                SubmitterName = comment.SubmitterName,
+                TargetLangauge = comment.TargetLanguage,
+                TranslateResponse = task.Result
+            };
+            return View(model);
+        }
+    }
+}
---
+++ AWS_Translate_Demo/AWS_Translate_Demo/Models/TranslateCommentViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.Translate.Model;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWS_Translate_Demo.Models
+{
+    public class TranslateCommentViewModel
+    {
+        public string SubmitterName { get; set; }
+        public string CommentText { get; set; }
+        public string TargetLanguage { get; set; }
+        public List<SelectListItem> TargetLanguages { get; set; }
+        public TranslateCommentViewModel()
+        {
+            // Set up the target language select
+            TargetLanguages = new List<SelectListItem>();
+            TargetLanguages.Add(new SelectListItem() { Text = "Select...", Value = string.Empty });
+            TargetLanguages.Add(new SelectListItem() { Text = "Chinese - Simplified (zh)", Value = "zh" });
+            TargetLanguages.Add(new SelectListItem() { Text = "French (fr)", Value = "fr" });
+            TargetLanguages.Add(new SelectListItem() { Text = "German (de)", Value = "de" });
+            TargetLanguages.Add(new SelectListItem() { Text = "Swedish (swe)", Value = "sv" });
+
+        }
+    }
+
+    public class TranslatedCommentViewModel
+    {
+        public string SubmitterName { get; set; }
+        public string CommentText { get; set; }
+        public string TargetLangauge { get; set; }
+        public TranslateTextResponse TranslateResponse { get; set; }
+    }
+}
