a9dc318243d7b03d2274490065001fd636beeaed
TheGameOfCode7 <vahiddaliri@gmail.com>
2021-04-25 20:30:31 +0200

    Test fix

--- CentiroHomeAssignment/CentiroHomeAssignment.Tests/CsvOrderMapperTests.cs
+++ CentiroHomeAssignment/CentiroHomeAssignment.Tests/CsvOrderMapperTests.cs
@@ -12,14 +12,14 @@ namespace CentiroHomeAssignment.Tests
         {
             List<string> orderElements = new List<string>
             {
-                "17835","0001","123-100","75","1x1 Red Piece","","0.05","Normal","2021-04-24","Kalle Svensson","265849"
+                "17835","0001","123-100","2","1x1 Red Piece","","0.05","Normal","2021-04-24","Kalle Svensson","265849"
             };
 
             var order = CsvOrderMapper.MapRow(orderElements);
 
             Assert.AreEqual(order.OrderNumber, "17835");
             Assert.AreEqual(order.ProductNumber, "123-100");
-            Assert.AreEqual(order.Quantity, "75");
+            Assert.AreEqual(order.Quantity, 2);
             Assert.AreEqual(order.Description, "");
             Assert.AreEqual(order.CustomerName, "Kalle Svensson");
             Assert.AreEqual(order.CustomerNumber, "265849");
